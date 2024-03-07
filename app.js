d44f5dfbb33904840e8fd0fda0efbbffceb91adc
Kat <69696346+a11yk4t@users.noreply.github.com>
2024-03-07 14:48:33 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3001
+const secret = process.env.SECRET
+
+import * as crypto from 'node:crypto'
+
+/**
+ * Verifies a Tiltify Webhook Signature is valid
+ *
+ * @param {string} secret
+ * @param {string} signature
+ * @param {string} timestamp
+ * @param {string} body
+ * @returns {boolean}
+ */
+const verifySignature = (secret, signature, timestamp, body) => {
+  const hmac = crypto.createHmac('sha256', secret)
+  hmac.update(`${timestamp}.${body}`)
+  return signature === hmac.digest('base64')
+}
 
 app.get("/", (req, res) => {
   res.send("hello world")
 })
 
 app.post("/hook", (req, res, next) => {
-  const webhookUrl = req.params.url
-  console.log(webhookUrl)
+  if (verifySignature(secret, req.get("X-Tiltify-Signature"), req.get("X-Tiltify-Timestamp"), req.body)) {
+    console.log("INVALID SIGNATURE")
+  } else {
+    console.log("VALID SIGNATURE")
+    console.log(req.body["data"]["amount"]["value"])
 
   res.status(200).send("OK")
 })
