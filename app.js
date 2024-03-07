b71b14832a5cdf5bd6aaf6e0e62d0b434e9a3b0e
Kat <69696346+a11yk4t@users.noreply.github.com>
2024-03-07 14:34:34 +0000

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.post("/hook", (req, res, next) => {
   console.log(webhookUrl)
 
   res.status(200).send("OK")
-}
+})
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
